1b35cfd35bcfa1a8e5cdd8f9e4133d7dc452bb4c
Tony Kwak <tkdev.to@gmail.com>
2024-03-12 10:46:33 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function (){
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function (){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+// // smooth scroll JS
+    $('nav a[href*="#"]').on('click', function (){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1500);
+    });
+
+    $('#up').on('click', function (){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1500);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    })
+
+
+//ghetto menu toggler
+// $(window).on('scroll', function(){
+//         $('.menu-toggler').toggleClass('hide')
+//     })
+
+});
+
---
+++ script.js
@@ -0,0 +1,53 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+let body = document.body;
+let toggleBtn = document.querySelector(".toggle-btn");
+let currentTheme = localStorage.getItem("currentTheme");
+
+if (currentTheme) {
+   body.classList.add("dark-theme");
+}
+toggleBtn.addEventListener("click", function () {
+   body.classList.toggle("dark-theme");
+
+   if (body.classList.contains("dark-theme")) {
+      localStorage.setItem("currentTheme", "themeActive");
+   } else {
+      localStorage.removeItem("currentTheme");
+   }
+});
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
